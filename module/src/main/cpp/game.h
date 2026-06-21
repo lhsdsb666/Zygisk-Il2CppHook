ad11b8f9e27ffa24c7a475e7e7186ff03552f6bf
lhsdsb666 <3388679639@qq.com>
2026-06-21 15:50:19 +0800

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,7 +5,7 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName1 "com.hypergryph.arknights.bilibili"
-#define GamePackageName2 "com.hypergryph.arknights"
+#define GamePackageName1 "com.epidgames.trickcalrevive"
+#define GamePackageName2 "com.epidgames.trickcalrevive"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
